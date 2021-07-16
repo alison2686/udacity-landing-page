72c6e67f379f5f5963146880c24e43759c946216
Alison Lee <alison2686@gmail.com>
2021-07-16 13:46:24 -0700

    setup app.js

---
+++ js/app.js
@@ -0,0 +1,66 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Define Global Variables
+ * 
+*/
+const nav = document.getElementById("navbar_list");
+const section = document.querySelectorAll("section");
+
+
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+const generateNav = () => {
+    section.forEach((section) => {
+        console.log(section)
+    })
+}
+
+generateNav();
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+
+// build the nav
+
+
+// Add class 'active' to section when near top of viewport
+
+
+// Scroll to anchor ID using scrollTO event
+
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+// Build menu 
+
+// Scroll to section on link click
+
+// Set sections as active
+
+
