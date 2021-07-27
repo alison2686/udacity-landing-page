be8a99beb798bfc824f55a371ef2d40164533185
Alison Lee <alison2686@gmail.com>
2021-07-27 10:55:01 -0700

    highlight section in view

--- js/app.js
+++ js/app.js
@@ -18,35 +18,28 @@
  * 
 */
 
-// nav variable gets navbar_list element id
+// nav variable gets navbar__list element by its id
 const nav = document.getElementById("navbar__list");
 
-// section variable selects each section element
+// section variable returns all section elements in the document
 const section = document.querySelectorAll("section");
-console.log(nav);
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
+
+// generateNav function gets data-nav and section id attributes, creates a <li> and appends newLi to the nav for each section
+// //set inner HTML to an <a> with an id of nav-sectionId, href of #sectionID and data-link to sectionName. Display sectionName
 const generateNav = () => {
     section.forEach((section) => {
-        // get data-nav attribute and set it to sectionName variable
         const sectionName = section.getAttribute("data-nav");
-        // get section id attribute and set to sectionId variable
         const sectionId = section.getAttribute("id");
-        // create a <li>
         const newLi = document.createElement("li");
-        // append newLi to nav
         nav.appendChild(newLi);
-        //set inner HTML to an <a> with href to sectionID and data-link to sectionName. Display sectionName
         newLi.innerHTML += `<a id="nav-${sectionId}" class="menu__link" href="#${sectionId}" data-link=${sectionName}">${sectionName}`;
-
-        // console.log(nav);
-        // console.log(newLi);
-        // console.log(sectionName);
-    })
+    });
 }
 
 /**
@@ -63,33 +56,38 @@ generateNav();
 // get bounding coordinates for each section
 const getSectionBounding = (section) => {
     const sectionPosition = section.getBoundingClientRect();
-    // console.log(sectionPosition)
     return sectionPosition;
 }
 
-// function to check each section's coordinates and determine if it's in the viewport
-// if section is in viewport add class "your-active-section" else remove class "your-active section"
+// function to check each section's bounding coordinates and determine if it's in the viewport
+// if section is in viewport add class "your-active-section" else remove it
+// if section is in viewport add class "active-nav-link" to nav link else remove it
 const addActiveClass = () => {
     section.forEach((section) => {
-        if (getSectionBounding(section).top < 100 && getSectionBounding(section).top >= -100) {
-            // add active-nav-link class to nav link
+        if (getSectionBounding(section).top < 150 && getSectionBounding(section).top >= -150) {
             let navId = section.getAttribute("id");
-            console.log(navId);
             document.querySelector(`a#nav-${navId}`).classList.add("active-nav-link");
-            // add your-active-class to section
             section.classList.add("your-active-class");
         } else {
-            // remove active-nav-link class from inactive nav link
             let navId = section.getAttribute("id");
             document.querySelector(`a#nav-${navId}`).classList.remove("active-nav-link");
-            // remove your-active-class from section
             section.classList.remove("your-active-class");
         }
     });
 };
 
-
 // Scroll to anchor ID using scrollTO event
+
+const scrollToSection = document.querySelectorAll("a");
+
+scrollToSection.forEach((link) =>
+    link.addEventListener("click", function(event) {
+        event.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    })
+);
 
 
 /**
@@ -102,7 +100,8 @@ const addActiveClass = () => {
 
 // Scroll to section on link click
 
+
 // Set sections as active
 window.addEventListener('scroll', addActiveClass);
-// window.addEventListener('scroll', addActiveNav);
+
 
